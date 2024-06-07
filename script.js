ad6d5924dc8905fa7be687648efaa11981ffd21f
erikdark <erikspichak@gmail.com>
2024-06-07 15:23:56 +0300

    Update script.js

--- script.js
+++ script.js
@@ -2,16 +2,30 @@
 const form = document.getElementById('registration-form');
 
 form.addEventListener('submit', (event) => {
-    event.preventDefault(); 
-    const username = document.getElementById('username').value;
-    const email = document.getElementById('email').value;
-    const password = document.getElementById('password').value;
+    event.preventDefault(); // Отменить стандартную отправку формы
+
+    const username = document.getElementById('username').value.trim();
+    const email = document.getElementById('email').value.trim();
+    const password = document.getElementById('password').value.trim();
+
+    // Проверка заполненности всех полей
+    if (username && email && password) {
+
+        const successMessage = document.getElementById('success-message');
+        successMessage.textContent = 'Вы успешно зарегистрированы!';
+        successMessage.style.display = 'block';
 
     
+        const errorMessage = document.getElementById('error-message');
+        errorMessage.style.display = 'none';
+    } else {
 
-    console.log('Данные формы:', {
-        username,
-        email,
-        password
-    });
+        const errorMessage = document.getElementById('error-message');
+        errorMessage.textContent = 'Пожалуйста, заполните все поля.';
+        errorMessage.style.display = 'block';
+
+        
+        const successMessage = document.getElementById('success-message');
+        successMessage.style.display = 'none';
+    }
 });
